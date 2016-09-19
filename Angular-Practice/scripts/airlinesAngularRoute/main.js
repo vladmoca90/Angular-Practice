c5510ebe12054fd65eab8efa4a6c5870986ef4f0
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-19 11:47:00 +0100

    Added the code for the main.js

--- Angular-Practice/scripts/airlinesAngularRoute/main.js
+++ Angular-Practice/scripts/airlinesAngularRoute/main.js
@@ -1,1 +1,9 @@
-﻿
+﻿var app = angular.module("myApp", ["ngRoute"]);
+
+app.controller("mainController", mainController)
+   .controller("airlineNameController", airlineNameController)
+   .controller("airlineCountryController", airlineCountryController)
+   .controller("airlineFleetController", airlineFleetController)
+   .controller("airlineDestinationsController", airlineDestinationsController)
+   .controller("errorController", errorController)
+   .config(routeSetup);
