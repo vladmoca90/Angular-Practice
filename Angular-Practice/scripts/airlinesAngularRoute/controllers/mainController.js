9edd239ccbe6e7c3d6f903363c569fe73e0662aa
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-19 11:56:03 +0100

    Added the code for the mainController.js

--- Angular-Practice/scripts/airlinesAngularRoute/controllers/mainController.js
+++ Angular-Practice/scripts/airlinesAngularRoute/controllers/mainController.js
@@ -1,1 +1,28 @@
-﻿
+﻿function mainController($scope, $http, $location) {
+
+    $scope.airlinesList = [
+        { airlineName: "LOT Polish", airlineCountry: "Poland", airlineFleet: "45", airlineDestinations: "60" },
+        { airlineName: "Brussels Airlines", airlineCountry: "Belgium", airlineFleet: "50", airlineDestinations: "76" },
+        { airlineName: "Thai Airways", airlineCountry: "Thailand", airlineFleet: "94", airlineDestinations: "80" },
+        { airlineName: "TAG Angola Airlines", airlineCountry: "Angola", airlineFleet: "13", airlineDestinations: "31" }
+    ];
+
+    $scope.goToAirlineName = function goToAirlineName(name) {
+        $location.path("/airlineName");
+    }
+
+    $scope.goToAirlineCountry = function goToAirlineCountry(country) {
+        $location.path("/airlineCountry");
+    }
+
+    $scope.goToAirlineFleet = function goToAirlineFleet(fleet) {
+        $location.path("/airlineFleet");
+    }
+
+    $scope.goToAirlineDestinations = function goToAirlineDestinations(destinations) {
+        $location.path("/airlineDestinations");
+    }
+
+}
+
+mainController.$inject = ["$scope", "$http", "$location"];
