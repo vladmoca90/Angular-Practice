4fbd8e431e638cb1f5d7a6f7cfa4bde84053c7a5
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-20 00:06:27 +0100

    Created the studentList array, with all the date about the students

--- Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
+++ Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
@@ -1,7 +1,13 @@
 ﻿var app = angular.module("studentApp", []);
 
-app.controller("studentController", function($scope) {
-    $scope.studentList = [
+app.controller("studentController", function ($scope) {
 
+    $scope.studentList = [
+        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
+        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
+        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
+        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
+        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science"}
     ];
+
 });
