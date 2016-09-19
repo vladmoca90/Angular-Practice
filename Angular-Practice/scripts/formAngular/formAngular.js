738ded4d8e7049844db99f71aff98e9c5d409e40
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-19 04:23:09 +0100

    Added the entire js code

--- Angular-Practice/scripts/formAngular/formAngular.js
+++ Angular-Practice/scripts/formAngular/formAngular.js
@@ -1,1 +1,65 @@
-﻿
+﻿var app = angular.module("formularApp", []);
+
+app.controller("formularController", function ($scope) {
+
+    $scope.user = {
+        firstName: "John",
+        lastName: "Thompson",
+        myEmail: "johnThompson1983@gmail.com",
+        myPassword: "aBc123ABc2000",
+        myTelephone: "07673307272",
+        myURL: "http://JohnThomp83.blogspot.co.uk",
+        descriptionSection: "Hello, my name is John Thompson and I am 33 years old. I am an American citizen that currently lives in Belfast, Northern Ireland and is looking for a job as a web developer. I speak French and Spanish at a professional level and German at an intermediate one."
+    };
+
+    $scope.Regions = [
+       {
+           Id: 1,
+           Name: "Select your area",
+           Selected: true
+       }, {
+           Id: 2,
+           Name: "European Union",
+           Selected: false
+       }, {
+           Id: 3,
+           Name: "UK/Ireland",
+           Selected: false
+       }, {
+           Id: 4,
+           Name: "United States",
+           Selected: false
+       }, {
+           Id: 5,
+           Name: "Canada",
+           Selected: false
+       }, {
+           Id: 6,
+           Name: "Australia/New Zealand",
+           Selected: false
+       }, {
+           Id: 7,
+           Name: "Africa",
+           Selected: false
+       }, {
+           Id: 7,
+           Name: "Asia",
+           Selected: false
+       },
+    ];
+
+    $scope.show = true;
+
+    $scope.formularShow = function formularShow() {
+        $scope.show = true;
+    }
+
+    $scope.formularHide = function formularHide() {
+        $scope.show = false;
+    }
+
+    $scope.formularToggle = function formularToggle() {
+        $scope.show = !$scope.show;
+    }
+
+});
