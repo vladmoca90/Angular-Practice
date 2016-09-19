bdcdea21b33616b0ccd822a3620dd5b2798dc4ee
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-19 11:46:21 +0100

    Added the entire code for the config.js

--- Angular-Practice/scripts/airlinesAngularRoute/config.js
+++ Angular-Practice/scripts/airlinesAngularRoute/config.js
@@ -1,1 +1,30 @@
-﻿
+﻿function routeSetup($routeProvider) {
+    $routeProvider
+    .when("/airlineName", {
+        templateUrl: "scripts/templates/airlineName.html",
+        controller: "airlineNameController"
+    })
+    .when("/airlineCountry", {
+        templateUrl: "scripts/templates/airlineCountry.html",
+        controller: "airlineCountryController"
+    })
+    .when("/airlineFleet", {
+        templateUrl: "scripts/templates/airlineFleet.html",
+        controller: "airlineFleetController"
+    })
+    .when("/airlineDestinations", {
+        templateUrl: "scripts/templates/airlineDestinations.html",
+        controller: "airlineDestinationsController"
+    })
+    .when("/error", {
+        templateUrl: "scripts/templates/error.html",
+        controller: "errorController"
+    })
+    .otherwise({
+        redirectTo: "/error"
+    });
+}
+
+routeSetup.$inject = [
+    "$routeProvider"
+];
