eec98b3e48c2c94d3355d647197c3ef120ba358a
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-19 04:25:52 +0100

    Changed a few details about the person included in the form

--- Angular-Practice/scripts/formAngular/formAngular.js
+++ Angular-Practice/scripts/formAngular/formAngular.js
@@ -3,13 +3,13 @@
 app.controller("formController", function ($scope) {
 
     $scope.user = {
-        firstName: "John",
+        firstName: "John Alexander",
         lastName: "Thompson",
-        myEmail: "johnThompson1983@gmail.com",
+        myEmail: "johnThompson2@gmail.com",
         myPassword: "aBc123ABc2000",
         myTelephone: "07673307272",
         myURL: "http://JohnThomp83.blogspot.co.uk",
-        descriptionSection: "Hello, my name is John Thompson and I am 33 years old. I am an American citizen that currently lives in Belfast, Northern Ireland and is looking for a job as a web developer. I speak French and Spanish at a professional level and German at an intermediate one."
+        descriptionSection: "Hello, my name is John Alexander Thompson and I am 33 years old. I am an American-Canadian citizen that currently lives in Belfast, Northern Ireland and is looking for a job as a web developer. I speak French and Spanish at a professional level and German at an intermediate one."
     };
 
     $scope.Regions = [
