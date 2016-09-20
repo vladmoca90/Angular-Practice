6823d214f17d9cb19a13e20650c502dfa515baa4
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-20 01:21:24 +0100

    Added countryFlag: " " in the studentsList

--- Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
+++ Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
@@ -3,13 +3,13 @@
 app.controller("studentController", function ($scope) {
 
     $scope.studentsList = [
-        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
-        { studentName: "Tatiana Smirnova", studentCountry: "Russia", studentAge: "29", studentDegree: "Bsc International Relations" },
-        { studentName: "Michael McDonald", studentCountry: "Canada", studentAge: "25", studentDegree: "Bsc Sport Sciences" },
-        { studentName: "Clodine Gonzales", studentCountry: "Philippines", studentAge: "23", studentDegree: "Bsc Psychology" },
-        { studentName: "Katrina van Bismarck", studentCountry: "Netherlands", studentAge: "20", studentDegree: "Bsc English Literature" },
-        { studentName: "Alexandru Sima", studentCountry: "Romania", studentAge: "25", studentDegree: "Bsc Medicine" },
-        { studentName: "Cai Hui", studentCountry: "China", studentAge: "22", studentDegree: "Bsc Geosciences" }
+        { studentName: "Omer Cerahoglu", countryFlag: "", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
+        { studentName: "Tatiana Smirnova", countryFlag: "", studentCountry: "Russia", studentAge: "29", studentDegree: "Bsc International Relations" },
+        { studentName: "Michael McDonald", countryFlag: "", studentCountry: "Canada", studentAge: "25", studentDegree: "Bsc Sport Sciences" },
+        { studentName: "Clodine Gonzales", countryFlag: "", studentCountry: "Philippines", studentAge: "23", studentDegree: "Bsc Psychology" },
+        { studentName: "Katrina van Bismarck", countryFlag: "", studentCountry: "Netherlands", studentAge: "20", studentDegree: "Bsc English Literature" },
+        { studentName: "Alexandru Sima", countryFlag: "", studentCountry: "Romania", studentAge: "25", studentDegree: "Bsc Medicine" },
+        { studentName: "Cai Hui", countryFlag: "", studentCountry: "China", studentAge: "22", studentDegree: "Bsc Geosciences" }
     ];
 
 });
