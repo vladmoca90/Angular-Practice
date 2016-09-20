bf12bf71410c212342ebd8c167746ca4484bf1d7
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-20 01:01:28 +0100

    Added new student names, ages, countries and degrees

--- Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
+++ Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
@@ -4,10 +4,10 @@ app.controller("studentController", function ($scope) {
 
     $scope.studentsList = [
         { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
-        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
-        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
-        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
-        { studentName: "Omer Cerahoglu", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science"}
+        { studentName: "Tatiana Smirnova", studentCountry: "Russia", studentAge: "29", studentDegree: "Bsc International Relations" },
+        { studentName: "Michael McDonald", studentCountry: "Canada", studentAge: "25", studentDegree: "Bsc Sport Sciences" },
+        { studentName: "Clodine Gonzales", studentCountry: "Philippines", studentAge: "23", studentDegree: "Bsc Psychology" },
+        { studentName: "Katrina van Bismarck", studentCountry: "Netherlands", studentAge: "20", studentDegree: "Bsc English Literature"}
     ];
 
 });
