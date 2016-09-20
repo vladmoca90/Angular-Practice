34315a82f219e056c014c027feb0081db9a73f0b
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-20 01:08:43 +0100

    Added 2 more students in the js code

--- Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
+++ Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
@@ -7,7 +7,10 @@ app.controller("studentController", function ($scope) {
         { studentName: "Tatiana Smirnova", studentCountry: "Russia", studentAge: "29", studentDegree: "Bsc International Relations" },
         { studentName: "Michael McDonald", studentCountry: "Canada", studentAge: "25", studentDegree: "Bsc Sport Sciences" },
         { studentName: "Clodine Gonzales", studentCountry: "Philippines", studentAge: "23", studentDegree: "Bsc Psychology" },
-        { studentName: "Katrina van Bismarck", studentCountry: "Netherlands", studentAge: "20", studentDegree: "Bsc English Literature"}
+        { studentName: "Katrina van Bismarck", studentCountry: "Netherlands", studentAge: "20", studentDegree: "Bsc English Literature" },
+        { studentName: "Alexandru Sima", studentCountry: "Romania", studentAge: "25", studentDegree: "Bsc Medicine" },
+        { studentName: "Cai Hui", studentCountry: "China", studentAge: "22", studentDegree: "Bsc Geosciences" }
+
     ];
 
 });
