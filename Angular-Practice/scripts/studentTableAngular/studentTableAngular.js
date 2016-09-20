d4163431f534c64056683244368e967e61cd01b5
Vlad Mocanu <vladmoca90@gmail.com>
2016-09-20 01:23:33 +0100

    Added the entire Urls for all flags

--- Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
+++ Angular-Practice/scripts/studentTableAngular/studentTableAngular.js
@@ -3,13 +3,13 @@
 app.controller("studentController", function ($scope) {
 
     $scope.studentsList = [
-        { studentName: "Omer Cerahoglu", countryFlag: "", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
-        { studentName: "Tatiana Smirnova", countryFlag: "", studentCountry: "Russia", studentAge: "29", studentDegree: "Bsc International Relations" },
-        { studentName: "Michael McDonald", countryFlag: "", studentCountry: "Canada", studentAge: "25", studentDegree: "Bsc Sport Sciences" },
-        { studentName: "Clodine Gonzales", countryFlag: "", studentCountry: "Philippines", studentAge: "23", studentDegree: "Bsc Psychology" },
-        { studentName: "Katrina van Bismarck", countryFlag: "", studentCountry: "Netherlands", studentAge: "20", studentDegree: "Bsc English Literature" },
-        { studentName: "Alexandru Sima", countryFlag: "", studentCountry: "Romania", studentAge: "25", studentDegree: "Bsc Medicine" },
-        { studentName: "Cai Hui", countryFlag: "", studentCountry: "China", studentAge: "22", studentDegree: "Bsc Geosciences" }
+        { studentName: "Omer Cerahoglu", countryFlag: "../../images/studentTableAngular/turkey.png", studentCountry: "Turkey", studentAge: "23", studentDegree: "Bsc Computer Science" },
+        { studentName: "Tatiana Smirnova", countryFlag: "../../images/studentTableAngular/russia.png", studentCountry: "Russia", studentAge: "29", studentDegree: "Bsc International Relations" },
+        { studentName: "Michael McDonald", countryFlag: "../../images/studentTableAngular/canada.png", studentCountry: "Canada", studentAge: "25", studentDegree: "Bsc Sport Sciences" },
+        { studentName: "Clodine Gonzales", countryFlag: "../../images/studentTableAngular/philippines.png", studentCountry: "Philippines", studentAge: "23", studentDegree: "Bsc Psychology" },
+        { studentName: "Katrina van Bismarck", countryFlag: "../../images/studentTableAngular/netherlands.png", studentCountry: "Netherlands", studentAge: "20", studentDegree: "Bsc English Literature" },
+        { studentName: "Alexandru Sima", countryFlag: "../../images/studentTableAngular/romania.png", studentCountry: "Romania", studentAge: "25", studentDegree: "Bsc Medicine" },
+        { studentName: "Cai Hui", countryFlag: "../../images/studentTableAngular/china.png", studentCountry: "China", studentAge: "22", studentDegree: "Bsc Geosciences" }
     ];
 
 });
