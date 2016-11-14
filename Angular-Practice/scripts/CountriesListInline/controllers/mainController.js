21c65f0bf7acd797729d340237697d8565dedde0
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-14 06:31:21 +0200

    Added the main code for the list and the ng-class

---
+++ Angular-Practice/scripts/CountriesListInline/controllers/mainController.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("countriesApp", []);
+
+app.controller("countriesCtrl", function ($scope) {
+
+    $scope.listInlineOfCountries = "list-inline";
+
+    $scope.CountriesList = {
+        name: "Germany",
+        name: "United Kingdom",
+        name: "France",
+        name: "Italy",
+        name: "Spain"
+    }
+
+});
