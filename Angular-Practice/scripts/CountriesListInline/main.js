02a7fe9eb0fc9a4c0f5a2225772ed9701699e809
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-14 06:49:57 +0200

    Created all the controllers in the main.js

--- Angular-Practice/scripts/CountriesListInline/main.js
+++ Angular-Practice/scripts/CountriesListInline/main.js
@@ -1,1 +1,9 @@
-﻿
+﻿var app = angular.module("countriesApp", ["ngRoute"]);
+
+app.controller("mainController", mainController)
+   .controller("GermanyController", GermanyController)
+   .controller("FranceController", FranceController)
+   .controller("UKController", UKController)
+   .controller("SpainController", SpainController)
+   .controller("ItalyController", ItalyController)
+   .controller("errorController", errorController)
