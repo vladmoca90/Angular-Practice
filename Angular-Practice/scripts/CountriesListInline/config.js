1f8bfa2d70c49e0693878456d371a25cbba067cc
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-14 08:25:09 +0200

    Added the config code to made the rounting

--- Angular-Practice/scripts/CountriesListInline/config.js
+++ Angular-Practice/scripts/CountriesListInline/config.js
@@ -1,12 +1,30 @@
 ﻿function routeSetup($routeProvier) {
     routePrivoder
-    .when()
-    .when()
-    .when()
-    .when()
-    .when()
-    .when()
+    .when("/Germany", {
+        templateUrl: "../../scripts/CountriesListInline/templates/Germany.html",
+        controller: "GermanyController"
+    })
+    .when("/UK", {
+        templateUrl: "../../scripts/CountriesListInline/templates/UK.html",
+        controller: "UKController"
+    })
+    .when("/France", {
+        templateUrl: "../../scripts/CountriesListInline/templates/France.html",
+        controller: "FranceController"
+    })
+    .when("/Italy", {
+        templateUrl: "../../scripts/CountriesListInline/templates/Italy.html",
+        controller: "ItalyController"
+    })
+    .when("/Spain", {
+        templateUrl: "../../scripts/CountriesListInline/templates/Spain.html",
+        controller: "SpainController"
+    })
+    .when("/error", {
+        templateUrl: "../../scripts/CountriesListInline/templates/error.html",
+        controller: "errorController"
+    })
     .otherwise({
-        redirectTo: "/error";
+        redirectTo: "/error"
     });
 }
