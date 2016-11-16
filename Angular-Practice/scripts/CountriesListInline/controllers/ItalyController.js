647e1ef632c025cf1736e4125a24fa3925e9a66f
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-16 16:41:40 +0000

    Created the function ItalyController()

--- Angular-Practice/scripts/CountriesListInline/controllers/ItalyController.js
+++ Angular-Practice/scripts/CountriesListInline/controllers/ItalyController.js
@@ -1,10 +1,5 @@
-﻿<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-	<meta charset="utf-8" />
-</head>
-<body>
+﻿function ItalyController($scope) {
 
-</body>
-</html>
+}
+
+ItalyController.$inject = [$scope];
