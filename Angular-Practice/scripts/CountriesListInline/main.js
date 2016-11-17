4be5eeca09e736f7db375a43448158ab3727aecc
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-17 23:02:28 +0000

    Saved the entire project with the compiled js files

--- Angular-Practice/scripts/CountriesListInline/main.js
+++ Angular-Practice/scripts/CountriesListInline/main.js
@@ -7,4 +7,4 @@ app.controller("mainController", mainController)
    .controller("SpainController", SpainController)
    .controller("ItalyController", ItalyController)
    .controller("errorController", errorController)
-    .config(routeSetup);
+   .config(routeSetup);
