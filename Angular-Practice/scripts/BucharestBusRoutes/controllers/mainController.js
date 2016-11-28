d64fee40630cc34eab2a44959d193b54f59018b3
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-28 22:50:53 +0000

    corrected into "toDestination"

--- Angular-Practice/scripts/BucharestBusRoutes/controllers/mainController.js
+++ Angular-Practice/scripts/BucharestBusRoutes/controllers/mainController.js
@@ -1,15 +1,17 @@
 ﻿function mainController($scope, $http) {
 
     $scope.BusesListHeadings = {
-        headingName: "Bus Number", headingTo: "Origin", headingFrom: "Destination"
+        headingName: "Bus Number",
+        headingTo: "Origin",
+        headingFrom: "Destination"
     }
 
     $scope.BusesList = [
-        { name: "104", toDeatination: "Piata Operei", fromDestination: "Stadion Lia Manoliu" },
-        { name: "131", toDeatination: "Piata Romana", fromDestination: "Complex Comercial Baneasa" },
-        { name: "205", toDeatination: "Gara de Nord", fromDestination: "Bioterra" },
-        { name: "301", toDeatination: "Piata Romana", fromDestination: "Perla" },
-        { name: "336", toDeatination: "Piata C.A. Rosetti", fromDestination: "Complex Apusului" }
+        { name: "104", toDestination: "Piata Operei", fromDestination: "Stadion Lia Manoliu" },
+        { name: "131", toDestination: "Piata Romana", fromDestination: "Complex Comercial Baneasa" },
+        { name: "205", toDestination: "Gara de Nord", fromDestination: "Bioterra" },
+        { name: "301", toDestination: "Piata Romana", fromDestination: "Perla" },
+        { name: "336", toDestination: "Piata C.A. Rosetti", fromDestination: "Complex Apusului" }
     ];
 
 }
