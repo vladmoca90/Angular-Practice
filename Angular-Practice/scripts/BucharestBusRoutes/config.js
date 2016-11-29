93c10abd4379232783c6fc8813a934612abdae57
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-29 08:36:57 +0000

    Added the configuration js code in config.js

--- Angular-Practice/scripts/BucharestBusRoutes/config.js
+++ Angular-Practice/scripts/BucharestBusRoutes/config.js
@@ -1,10 +1,29 @@
-﻿var app = angular.module("BucharestApp", ["ngRoute"]);
+﻿
+function routeSetup($routeProvider) {
+    $routeProvider
+    .when("/airlineName", {
+        templateUrl: "../../scripts/airlinesAngularRoute/templates/airlineName.html",
+        controller: "airlineNameController"
+    })
+    .when("/airlineCountry", {
+        templateUrl: "../../scripts/airlinesAngularRoute/templates/airlineCountry.html",
+        controller: "airlineCountryController"
+    })
+    .when("/airlineFleet", {
+        templateUrl: "../../scripts/airlinesAngularRoute/templates/airlineFleet.html",
+        controller: "airlineFleetController"
+    })
+    .when("/airlineDestinations", {
+        templateUrl: "../../scripts/airlinesAngularRoute/templates/airlineDestinations.html",
+        controller: "airlineDestinationsController"
+    })
+    .when("/error", {
+        templateUrl: "../../scripts/airlinesAngularRoute/templates/error.html",
+        controller: "errorController"
+    })
+    .otherwise({
+        redirectTo: "/error"
+    });
+}
 
-app.controller("mainController", mainController)
-   .controller("bus104controller", bus104controller)
-   .controller("bus131controller", bus131controller)
-   .controller("bus205controller", bus205controller)
-   .controller("bus301controller", bus301controller)
-   .controller("bus336controller", bus336controller)
-   .controller("errorController", errorController)
-   .config(routeSetup);
+routeSetup.$inject = ["$routeProvider"];
