1268794763092dcaa120f6230c1d3c9be748ec7f
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-29 19:22:46 +0000

    Put all the properties one under the other

--- Angular-Practice/scripts/RomanianHighschoolExamResults/RomanianHighschoolExamResults.js
+++ Angular-Practice/scripts/RomanianHighschoolExamResults/RomanianHighschoolExamResults.js
@@ -3,13 +3,62 @@
 app.controller("examResultsController", function ($scope) {
 
     $scope.resultsList = [
-        { name: "Vlad Mocanu", romanianSubject: "8.80", mathsSubject: "8.60", thirdSubjectName: "Biology", thirdSubjectResult: "9.80", status: "Pass" },
-        { name: "Simionescu Horatiu", romanianSubject: "7.20", mathsSubject: "5.45", thirdSubjectName: "Physics", thirdSubjectResult: "6.95", status: "Pass" },
-        { name: "Marinescu Leonard", romanianSubject: "8.95", mathsSubject: "3.90", thirdSubjectName: "Biology", thirdSubjectResult: "9.65", status: "Fail" },
-        { name: "Alexandru Mocanu", romanianSubject: "8.10", mathsSubject: "8.70", thirdSubjectName: "Informatics", thirdSubjectResult: "9.75", status: "Pass" },
-        { name: "Dumitru Costel", romanianSubject: "4.25", mathsSubject: "5.60", thirdSubjectName: "Biology", thirdSubjectResult: "5.95", status: "Fail" },
-        { name: "Nedelcu Ruxandra", romanianSubject: "9.65", mathsSubject: "9.50", thirdSubjectName: "Chemistry", thirdSubjectResult: "10", status: "Pass" },
-        { name: "Despa Francesca", romanianSubject: "9.80", mathsSubject: "9.15", thirdSubjectName: "Informatics", thirdSubjectResult: "9.90", status: "Pass" }
+        {
+            name: "Vlad Mocanu",
+            romanianSubject: "8.80",
+            mathsSubject: "8.60",
+            thirdSubjectName: "Biology",
+            thirdSubjectResult: "9.80",
+            status: "Pass"
+        },
+        {
+            name: "Simionescu Horatiu",
+            romanianSubject: "7.20",
+            mathsSubject: "5.45",
+            thirdSubjectName: "Physics",
+            thirdSubjectResult: "6.95",
+            status: "Pass"
+        },
+        {
+            name: "Marinescu Leonard",
+            romanianSubject: "8.95",
+            mathsSubject: "3.90",
+            thirdSubjectName: "Biology",
+            thirdSubjectResult: "9.65",
+            status: "Fail"
+        },
+        {
+            name: "Alexandru Mocanu",
+            romanianSubject: "8.10",
+            mathsSubject: "8.70",
+            thirdSubjectName: "Informatics",
+            thirdSubjectResult: "9.75",
+            status: "Pass"
+        },
+        {
+            name: "Dumitru Costel",
+            romanianSubject: "4.25",
+            mathsSubject: "5.60",
+            thirdSubjectName: "Biology",
+            thirdSubjectResult: "5.95",
+            status: "Fail"
+        },
+        {
+            name: "Nedelcu Ruxandra",
+            romanianSubject: "9.65",
+            mathsSubject: "9.50",
+            thirdSubjectName: "Chemistry",
+            thirdSubjectResult: "10",
+            status: "Pass"
+        },
+        {
+            name: "Despa Francesca",
+            romanianSubject: "9.80",
+            mathsSubject: "9.15",
+            thirdSubjectName: "Informatics",
+            thirdSubjectResult: "9.90",
+            status: "Pass"
+        }
     ];
 
 });
