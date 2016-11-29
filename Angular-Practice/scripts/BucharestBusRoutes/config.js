e09b0fd94b3363c5d0bb76d0fd7c81978a59efdc
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-29 11:43:24 +0000

    Added BucharestBusRoutes as the source for the templates

--- Angular-Practice/scripts/BucharestBusRoutes/config.js
+++ Angular-Practice/scripts/BucharestBusRoutes/config.js
@@ -2,27 +2,27 @@
 function routeSetup($routeProvider) {
     $routeProvider
     .when("/bus104", {
-        templateUrl: "../../scripts/airlinesAngularRoute/templates/bus104.html",
+        templateUrl: "../../scripts/BucharestBusRoutes/templates/bus104.html",
         controller: "bus104Controller"
     })
     .when("/bus131", {
-        templateUrl: "../../scripts/airlinesAngularRoute/templates/bus131.html",
+        templateUrl: "../../scripts/BucharestBusRoutes/templates/bus131.html",
         controller: "bus131Controller"
     })
     .when("/bus205", {
-        templateUrl: "../../scripts/airlinesAngularRoute/templates/bus205.html",
+        templateUrl: "../../scripts/BucharestBusRoutes/templates/bus205.html",
         controller: "bus205Controller"
     })
     .when("/bus301", {
-        templateUrl: "../../scripts/airlinesAngularRoute/templates/bus301.html",
+        templateUrl: "../../scripts/BucharestBusRoutes/templates/bus301.html",
         controller: "bus301Controller"
     })
     .when("/bus336", {
-        templateUrl: "../../scripts/airlinesAngularRoute/templates/bus336.html",
+        templateUrl: "../../scripts/BucharestBusRoutes/templates/bus336.html",
         controller: "bus336Controller"
      })
     .when("/error", {
-        templateUrl: "../../scripts/airlinesAngularRoute/templates/error.html",
+        templateUrl: "../../scripts/BucharestBusRoutes/templates/error.html",
         controller: "errorController"
     })
     .otherwise({
