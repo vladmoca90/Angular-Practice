2ddaaf4161c5302bb229bce8e91f6be5aacccd26
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-29 12:43:16 +0000

    Added some samples in the resultsList

--- Angular-Practice/scripts/RomanianHighschoolExamResults/RomanianHighschoolExamResults.js
+++ Angular-Practice/scripts/RomanianHighschoolExamResults/RomanianHighschoolExamResults.js
@@ -2,8 +2,14 @@
 
 app.controller("examResultsController", function ($scope) {
 
-    $scope.resultsList = {
-
-    }
+    $scope.resultsList = [
+        { name: "Vlad Mocanu", romanianSubject: "8.80", mathsSubject: "8.60", thirdSubjectName: "Biology", thirdSubjectResult: "9.80", status: "Pass" },
+        { name: "Vlad Mocanu", romanianSubject: "8.80", mathsSubject: "8.60", thirdSubjectName: "Biology", thirdSubjectResult: "9.80", status: "Pass" },
+        { name: "Vlad Mocanu", romanianSubject: "8.80", mathsSubject: "8.60", thirdSubjectName: "Biology", thirdSubjectResult: "9.80", status: "Pass" },
+        { name: "Vlad Mocanu", romanianSubject: "8.80", mathsSubject: "8.60", thirdSubjectName: "Biology", thirdSubjectResult: "9.80", status: "Pass" },
+        { name: "Vlad Mocanu", romanianSubject: "8.80", mathsSubject: "8.60", thirdSubjectName: "Biology", thirdSubjectResult: "9.80", status: "Pass" },
+        { name: "Vlad Mocanu", romanianSubject: "8.80", mathsSubject: "8.60", thirdSubjectName: "Biology", thirdSubjectResult: "9.80", status: "Pass" },
+        { name: "Vlad Mocanu", romanianSubject: "8.80", mathsSubject: "8.60", thirdSubjectName: "Biology", thirdSubjectResult: "9.80", status: "Pass" }
+    ];
 
 });
