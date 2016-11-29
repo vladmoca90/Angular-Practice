cb71c1d5c10d568ae66342e890d70a2bf81b1a50
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-29 08:37:30 +0000

    Moved the main code from config.js into main.js

--- Angular-Practice/scripts/BucharestBusRoutes/main.js
+++ Angular-Practice/scripts/BucharestBusRoutes/main.js
@@ -1,1 +1,10 @@
-﻿
+﻿var app = angular.module("BucharestApp", ["ngRoute"]);
+
+app.controller("mainController", mainController)
+   .controller("bus104controller", bus104controller)
+   .controller("bus131controller", bus131controller)
+   .controller("bus205controller", bus205controller)
+   .controller("bus301controller", bus301controller)
+   .controller("bus336controller", bus336controller)
+   .controller("errorController", errorController)
+   .config(routeSetup);
