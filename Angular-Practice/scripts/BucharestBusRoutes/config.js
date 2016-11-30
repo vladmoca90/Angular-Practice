c0cabff98b8cbc7356a72e0e854fe48709db9f65
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-30 23:25:04 +0000

    Corrected the names of the controllers to be with lowercase "c"

--- Angular-Practice/scripts/BucharestBusRoutes/config.js
+++ Angular-Practice/scripts/BucharestBusRoutes/config.js
@@ -2,23 +2,23 @@
     $routeProvider
     .when("/bus104", {
         templateUrl: "../../scripts/BucharestBusRoutes/templates/bus104.html",
-        controller: "bus104Controller"
+        controller: "bus104controller"
     })
     .when("/bus131", {
         templateUrl: "../../scripts/BucharestBusRoutes/templates/bus131.html",
-        controller: "bus131Controller"
+        controller: "bus131controller"
     })
     .when("/bus205", {
         templateUrl: "../../scripts/BucharestBusRoutes/templates/bus205.html",
-        controller: "bus205Controller"
+        controller: "bus205controller"
     })
     .when("/bus301", {
         templateUrl: "../../scripts/BucharestBusRoutes/templates/bus301.html",
-        controller: "bus301Controller"
+        controller: "bus301controller"
     })
     .when("/bus336", {
         templateUrl: "../../scripts/BucharestBusRoutes/templates/bus336.html",
-        controller: "bus336Controller"
+        controller: "bus336controller"
      })
     .when("/error", {
         templateUrl: "../../scripts/BucharestBusRoutes/templates/error.html",
