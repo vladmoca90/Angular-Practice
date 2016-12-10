9955fcb9ef9104bdd85187aff517c8c12e6b2df4
Vlad Mocanu <vladmoca90@gmail.com>
2016-12-10 14:38:02 +0000

    Created the code for $scope.airlineList = {[ ]}

--- Angular-Practice/scripts/airlineTableAngular/airlineTableAngular.js
+++ Angular-Practice/scripts/airlineTableAngular/airlineTableAngular.js
@@ -3,10 +3,17 @@
 app.controller("airlineTableCtrl", function () {
 
      $scope.headingList = [
-         headingName: "Name",
-         headingCountry: "Country",
-         headingDestinations: "Destinations",
-         headingFleet: "Fleet"
-     ];
+        headingName: "Name",
+        headingCountry: "Country",
+        headingDestinations: "Destinations",
+        headingFleet: "Fleet"
+    ];
+
+    $scope.airlineList = [
+        airlineName: "British Airways",
+        airlineCountry: "United Kingdom",
+        airlineDestinations: "",
+        airlineFleet: "",
+    ];
 
 });
