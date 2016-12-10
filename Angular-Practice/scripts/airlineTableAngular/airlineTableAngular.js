ef774ee4cb18653a596e7b5a35b61a4cbbf81bdf
Vlad Mocanu <vladmoca90@gmail.com>
2016-12-10 15:03:36 +0000

    Added 4 more airlines to the list

--- Angular-Practice/scripts/airlineTableAngular/airlineTableAngular.js
+++ Angular-Practice/scripts/airlineTableAngular/airlineTableAngular.js
@@ -11,8 +11,11 @@ app.controller("airlineTableCtrl", function () {
 
     $scope.airlineList = [
         { airlineName: "British Airways", airlineCountry: "United Kingdom", airlineDestinations: "", airlineFleet: ""},
-        { airlineName: "British Airways", airlineCountry: "United Kingdom", airlineDestinations: "", airlineFleet: ""},
-        { airlineName: "British Airways", airlineCountry: "United Kingdom", airlineDestinations: "", airlineFleet: ""}
+        { airlineName: "Air France", airlineCountry: "France", airlineDestinations: "", airlineFleet: ""},
+        { airlineName: "Iberia", airlineCountry: "Spain", airlineDestinations: "", airlineFleet: ""},
+        { airlineName: "Air Serbia", airlineCountry: "Serbia", airlineDestinations: "", airlineFleet: ""},
+        { airlineName: "Swissair", airlineCountry: "Switzerland", airlineDestinations: "", airlineFleet: ""}
+
     ];
 
 });,
