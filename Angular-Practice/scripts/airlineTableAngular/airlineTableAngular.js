a263bf105a2f4d0867868890e92aa42b8136409d
Vlad Mocanu <vladmoca90@gmail.com>
2016-12-10 20:19:42 +0000

    Changed the [ ] into { } for the $scope.headingList

--- Angular-Practice/scripts/airlineTableAngular/airlineTableAngular.js
+++ Angular-Practice/scripts/airlineTableAngular/airlineTableAngular.js
@@ -2,12 +2,12 @@
 
 app.controller("airlineTableCtrl", function () {
 
-     $scope.headingList = [
+    $scope.headingList = {
         headingName: "Name",
         headingCountry: "Country",
         headingDestinations: "Destinations",
         headingFleet: "Fleet"
-    ];
+    }
 
     $scope.airlineList = [
         { airlineName: "British Airways", airlineCountry: "United Kingdom", airlineDestinations: "", airlineFleet: ""},
