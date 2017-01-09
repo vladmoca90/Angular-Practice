8f982086f3aef7e25a584f333f8905a6730c836b
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-09 11:54:58 +0000

    Deleted the lines that had the code for the $location.path()

--- Angular-Practice/scripts/CountriesListInline/controllers/mainController.es5.js
+++ Angular-Practice/scripts/CountriesListInline/controllers/mainController.es5.js
@@ -5,26 +5,6 @@ function mainController($scope, $http, $location) {
     $scope.tableProperties = "table table-bordered";
 
     $scope.CountriesList = [{ countryName: "Germany" }, { countryName: "United Kingdom" }, { countryName: "France" }, { countryName: "Italy" }, { countryName: "Spain" }];
-
-    $scope.goToCountryGermany = function goToCountryGermany(germany) {
-        $location.path("/Germany");
-    };
-
-    $scope.goToCountryUK = function goToCountryUK(uk) {
-        $location.path("/UK");
-    };
-
-    $scope.goToCountryFrance = function goToCountryFrance(france) {
-        $location.path("/France");
-    };
-
-    $scope.goToCountryItaly = function goToCountryItaly(italy) {
-        $location.path("/Italy");
-    };
-
-    $scope.goToCountrySpain = function goToCountrySpain(spain) {
-        $location.path("/Spain");
-    };
 }
 
 mainController.$inject = ["$scope", "$http", "$location"];
--- Angular-Practice/scripts/CountriesListInline/controllers/mainController.js
+++ Angular-Practice/scripts/CountriesListInline/controllers/mainController.js
@@ -10,26 +10,6 @@
         { countryName: "Spain" }
     ];
 
-    $scope.goToCountryGermany = function goToCountryGermany(germany) {
-        $location.path("/Germany");
-    }
-
-    $scope.goToCountryUK = function goToCountryUK(uk) {
-        $location.path("/UK");
-    }
-
-    $scope.goToCountryFrance = function goToCountryFrance(france) {
-        $location.path("/France");
-    }
-
-    $scope.goToCountryItaly = function goToCountryItaly(italy) {
-        $location.path("/Italy");
-    }
-
-    $scope.goToCountrySpain = function goToCountrySpain(spain) {
-        $location.path("/Spain");
-    }
-
 }
 
 mainController.$inject = ["$scope", "$http", "$location"];
