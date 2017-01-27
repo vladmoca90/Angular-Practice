075d2a7d24759acc74f9eb0ff739daea8894d084
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-27 23:31:00 +0000

    Added the max-width and the font styles of the "countriesListInlineStyles" in AngularJS

--- Angular-Practice/scripts/CountriesListInline/countriesListInline.js
+++ Angular-Practice/scripts/CountriesListInline/countriesListInline.js
@@ -2,7 +2,13 @@
 
 app.controller("countriesController", function ($scope) {
 
-    $scope.tableProperties = "table table-bordered"
+    $scope.tableProperties = "table table-bordered";
+
+    $scope.countriesListInlineStyles = {
+        "max-width": "500px",
+        "font-size": "18px",
+        "font-family": "Ubuntu"
+    }
 
     $scope.CountriesList = [
         { countryName: "Germany" },
