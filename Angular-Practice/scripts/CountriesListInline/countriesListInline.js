d2b7fdeae9e481e937e61eb2d7630e319c1c60cc
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-27 21:03:34 +0000

    Created countriesListInline.js

---
+++ Angular-Practice/scripts/CountriesListInline/countriesListInline.js
@@ -0,0 +1,15 @@
+﻿function mainController($scope, $http, $location) {
+
+    $scope.tableProperties = "table table-bordered"
+
+    $scope.CountriesList = [
+        { countryName: "Germany" },
+        { countryName: "United Kingdom" },
+        { countryName: "France" },
+        { countryName: "Italy" },
+        { countryName: "Spain" }
+    ];
+
+}
+
+mainController.$inject = ["$scope", "$http", "$location"];
