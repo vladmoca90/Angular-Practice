c0928a61542b752a0753e895601c74754e4a6ca6
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-29 00:37:16 +0000

    CReated the configuration code in main.js

--- Angular-Practice/scripts/USPresidentsRouting/main.js
+++ Angular-Practice/scripts/USPresidentsRouting/main.js
@@ -1,1 +1,8 @@
-﻿
+﻿var app = angular.module("USPresidentsApp", ["ngRoute"]);
+
+app.controller("mainController", mainController)
+   .controller("BushController", BushController)
+   .controller("ObamaController", ObamaController)
+   .controller("ClintonController", ClintonController)
+   .controller("RaeganController", RaeganController)
+   .config(routeSetup);
