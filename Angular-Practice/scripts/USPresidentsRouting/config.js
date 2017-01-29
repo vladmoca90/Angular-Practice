b7c3d70e573a4731db40826e8c639114bfa300b2
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-29 00:48:32 +0000

    Created the code for the routeProvider

--- Angular-Practice/scripts/USPresidentsRouting/config.js
+++ Angular-Practice/scripts/USPresidentsRouting/config.js
@@ -1,1 +1,28 @@
-﻿
+﻿function routeSetup($routeProvider) {
+    $routeProvider
+    .when("/Bush", {
+        templateUrl: "../../scripts/USPresidentsRouting/templates/Bush.html",
+        controller: "BushController"
+    })
+    .when("/Clinton", {
+        templateUrl: "../../scripts/USPresidentsRouting/templates/Clinton.html",
+        controller: "ClintonController"
+    })
+    .when("/Obama", {
+        templateUrl: "../../scripts/USPresidentsRouting/templates/Obama.html",
+        controller: "ObamaController"
+    })
+    .when("/Raegan", {
+        templateUrl: "../../scripts/USPresidentsRouting/templates/Raegan.html",
+        controller: "RaeganController"
+    })
+    .when("/error", {
+        templateUrl: "../../scripts/BucharestBusRoutes/templates/error.html",
+        controller: "errorController"
+    })
+    .otherwise({
+        redirectTo: "/error"
+    });
+}
+
+routeSetup.$inject = ["$routeProvider"];
