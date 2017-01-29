a580e67464a6651c9d5f2dbbec1ccb5c77692ada
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-29 17:03:57 +0000

    Created the pizzaController and create the $scope.count

--- Angular-Practice/scripts/Pizzas/controllers/pizzasController.js
+++ Angular-Practice/scripts/Pizzas/controllers/pizzasController.js
@@ -1,5 +1,36 @@
-﻿app.controller("pizzasController", function ($scope) {
+﻿function pizzasController($scope) {
 
+    $scope.pizzas = [
+        {
+            count: 0
+        },
+        {
+            count: 0
+        },
+        {
+            count: 0
+        }
+    ];
 
+    $scope.decrease = function (pizza) {
+        if (pizza.count == 0) {
+            return;
+        }
+        pizza.count--;
 
-});
+        // remove pizza from basket or decrease count if more than 1
+    }
+
+    $scope.increase = function (pizza) {
+        pizza.count++;
+
+        // addpizza to basket, but first check if it's already there
+    }
+
+    $scope.basket = {
+        items: [],
+        price: 0
+    }
+}
+
+pizzasController.$inject = ["$scope"];
