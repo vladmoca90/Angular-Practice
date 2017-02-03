c513b2bb18e434d0e0b1bd3d2cb297f42661e7ab
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-03 22:08:42 +0000

    Created the function formController($scope) { }

---
+++ Angular-Practice/scripts/formAngular/controller/formAngularOneController.js
@@ -0,0 +1,59 @@
+﻿function formController($scope) {
+
+    $scope.groupsOfFormsItems = "form-group";
+
+    $scope.formControlClass = "form-control";
+
+    $scope.user = {
+        firstName: "John Alexander",
+        lastName: "Thompson",
+        myEmail: "johnThompson2@gmail.com",
+        myPassword: "aBc123ABc2000",
+        myTelephone: "07673307272",
+        myURL: "http://JohnThomp83.blogspot.co.uk",
+        descriptionSection: "Hello, my name is John Alexander Thompson and I am 33 years old. I am an American-Canadian citizen that currently lives in Belfast, Northern Ireland and is looking for a job as a web developer. I speak French and Spanish at a professional level and German at an intermediate one."
+    };
+
+    $scope.regionsOptions = [
+        {
+            Id: 1,
+            Name: "European Union",
+            Selected: false
+        }, {
+            Id: 2,
+            Name: "UK/Ireland",
+            Selected: true
+        }, {
+            Id: 3,
+            Name: "United States",
+            Selected: false
+        }, {
+            Id: 4,
+            Name: "Canada",
+            Selected: false
+        }, {
+            Id: 5,
+            Name: "Australia/New Zealand",
+            Selected: false
+        }, {
+            Id: 6,
+            Name: "South Africa",
+            Selected: false
+        }, {
+            Id: 7,
+            Name: "Hong Kong",
+            Selected: false
+        },
+       {
+           Id: 8,
+           Name: "Japan/South Korea/China/Taiwan",
+           Selected: false
+       },
+       {
+           Id: 9,
+           Name: "Commonwealth of Independent Nations",
+           Selected: false
+       }
+    ];
+
+}
