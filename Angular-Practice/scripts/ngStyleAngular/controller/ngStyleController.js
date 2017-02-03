69859d80f3a511439cb78255ceca11baba25c9d2
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-03 22:05:28 +0000

    Created the function citiesCtrl($scope) { }

--- Angular-Practice/scripts/ngStyleAngular/controller/ngStyleController.js
+++ Angular-Practice/scripts/ngStyleAngular/controller/ngStyleController.js
@@ -1,1 +1,43 @@
-﻿
+﻿function citiesCtrl($scope) {
+
+    $scope.tableType = "table table-bordered";
+
+    $scope.citiesBackground = {
+        "background": "#A1A1A1",
+        "color": "#FFFFFF",
+        "font-weight": "bold"
+    }
+
+    $scope.tableStyles = {
+        "width": "50%",
+        "margin": "3% auto",
+        "text-align": "center"
+    }
+
+    $scope.parisStyle = {
+        "font-size": "18px",
+        "color": "red",
+        "font-weight": "bold"
+    }
+
+    $scope.londonStyle = {
+        "font-style": "italic",
+        "color": "green",
+        "font-size": "18px"
+    }
+
+    $scope.newYorkStyle = {
+        "color": "orange",
+        "font-family": "Verdana",
+        "font-size": "15px",
+        "font-weight": "500"
+    }
+
+    $scope.madridStyle = {
+        "color": "purple",
+        "letter-spacing": "3px",
+        "font-family": "Andalus",
+        "font-size": "20px"
+    }
+
+}
