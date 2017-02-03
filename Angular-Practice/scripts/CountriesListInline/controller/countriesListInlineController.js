cd9f89a772f9d25f2d26da5a42ca30004e69500f
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-03 22:13:05 +0000

    Created the function countriesController($scope) { }

---
+++ Angular-Practice/scripts/CountriesListInline/controller/countriesListInlineController.js
@@ -0,0 +1,26 @@
+﻿function countriesController($scope) {
+
+    $scope.tableProperties = "table table-bordered";
+
+    $scope.countriesListInlineStyles = {
+        "max-width": "500px",
+        "font-size": "18px",
+        "font-family": "Ubuntu",
+        "margin": "2% auto"
+    }
+
+    $scope.countriesTdStyles = {
+        "padding": "10px 15px",
+        "color": "#000000",
+        "text-decoration": "none"
+    }
+
+    $scope.countries = [
+        { countryName: "Germany" },
+        { countryName: "United Kingdom" },
+        { countryName: "France" },
+        { countryName: "Italy" },
+        { countryName: "Spain" }
+    ];
+
+}
--- Angular-Practice/scripts/CountriesListInline/countriesListInline.js
+++ Angular-Practice/scripts/CountriesListInline/countriesListInline.js
@@ -1,28 +1,3 @@
 ﻿var app = angular.module("countriesApp", []);
 
-app.controller("countriesController", function ($scope) {
-
-    $scope.tableProperties = "table table-bordered";
-
-    $scope.countriesListInlineStyles = {
-        "max-width": "500px",
-        "font-size": "18px",
-        "font-family": "Ubuntu",
-        "margin": "2% auto"
-    }
-
-    $scope.countriesTdStyles = {
-        "padding": "10px 15px",
-        "color": "#000000",
-        "text-decoration": "none"
-    }
-
-    $scope.CountriesList = [
-        { countryName: "Germany" },
-        { countryName: "United Kingdom" },
-        { countryName: "France" },
-        { countryName: "Italy" },
-        { countryName: "Spain" }
-    ];
-
-});
+app.controller("countriesController", countriesController);
