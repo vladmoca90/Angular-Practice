8ec0c0183a17d6b1714cf4431d020bd916c9da5f
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-06 20:49:47 +0000

    Added a top margin of 2% and changed the wodth and height to 450px

--- Angular-Practice/scripts/ngHideShow/ngHideShowController.js
+++ Angular-Practice/scripts/ngHideShow/ngHideShowController.js
@@ -3,10 +3,11 @@
     $scope.boxPosition = "center-block";
 
     $scope.mainBox = {
-        "width": "600px",
-        "height": "600px",
+        "width": "450px",
+        "height": "450px",
         "background": "gold",
-        "border": "1px solid #000"
+        "border": "1px solid #000",
+        "margin-top": "2%"
     }
 
 }
