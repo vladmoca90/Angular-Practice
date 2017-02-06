642313ca82c2019710840a8b9a1a37943a8d00c9
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-06 21:03:58 +0000

    CReated the two buttons and added two classes on the with ng-class=" "

--- Angular-Practice/scripts/ngHideShow/controller/ngHideShowController.js
+++ Angular-Practice/scripts/ngHideShow/controller/ngHideShowController.js
@@ -2,6 +2,10 @@
 
     $scope.boxPosition = "center-block";
 
+    $scope.showButton = "btn btn-success";
+
+    $scope.hideButton = "btn btn-danger";
+
     $scope.mainBox = {
         "width": "450px",
         "height": "450px",
