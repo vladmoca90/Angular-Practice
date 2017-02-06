991fe0c3b61b9de1852e97183019dc9c4431e66f
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-06 20:45:05 +0000

    Created ng-style="mainBox" and added style to it: width, height, background and border and an ng-class with "center-block"

--- Angular-Practice/scripts/ngHideShow/ngHideShowController.js
+++ Angular-Practice/scripts/ngHideShow/ngHideShowController.js
@@ -1,5 +1,14 @@
 ﻿function mainCtrl($scope) {
 
+    $scope.boxPosition = "center-block";
+
+    $scope.mainBox = {
+        "width": "600px",
+        "height": "600px",
+        "background": "gold",
+        "border": "1px solid #000"
+    }
+
 }
 
 mainCtrl.$inject = ["$scope"];
