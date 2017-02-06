3070e070aba155d5703d848b8a8a94797764dfce
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-06 23:00:38 +0000

    Added a width and height of 300px to mainBox

--- Angular-Practice/scripts/ngHideShow/controller/ngHideShowController.js
+++ Angular-Practice/scripts/ngHideShow/controller/ngHideShowController.js
@@ -7,8 +7,8 @@
     $scope.hideButton = "btn btn-danger";
 
     $scope.mainBox = {
-        "width": "450px",
-        "height": "450px",
+        "width": "300px",
+        "height": "300px",
         "background": "gold",
         "border": "1px solid #000",
         "margin-top": "2%"
