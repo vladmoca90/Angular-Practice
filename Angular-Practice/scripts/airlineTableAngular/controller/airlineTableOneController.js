4c1620b07c1b632c768d004d6e238ca41a1c153e
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-23 22:52:17 +0000

    Started the remake eveyrthing with the controllers in a separate folder

---
+++ Angular-Practice/scripts/airlineTableAngular/controller/airlineTableOneController.js
@@ -0,0 +1,16 @@
+﻿function airlineTableOneController($scope) {
+
+    $scope.tableProperties = "table table-bordered";
+
+    $scope.airlines = [
+        { airlineName: "British Airways", airlineCountry: "United Kingdom", airlineDestinations: "183", airlineFleet: "297" },
+        { airlineName: "Air France", airlineCountry: "France", airlineDestinations: "204", airlineFleet: "235" },
+        { airlineName: "Iberia", airlineCountry: "Spain", airlineDestinations: "89", airlineFleet: "79" },
+        { airlineName: "Air Serbia", airlineCountry: "Serbia", airlineDestinations: "44", airlineFleet: "21" },
+        { airlineName: "Swiss International Air Lines", airlineCountry: "Switzerland", airlineDestinations: "106", airlineFleet: "68" }
+    ];
+
+}
+
+airlineTableController.$inject = ["$scope"];
+
