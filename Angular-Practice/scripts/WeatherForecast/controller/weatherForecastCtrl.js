d31b013137bb445b7ca8aa1f1fc1af9aa19447d7
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-21 12:29:17 +0000

    Added some properties to the $scope.weather

---
+++ Angular-Practice/scripts/WeatherForecast/controller/weatherForecastCtrl.js
@@ -0,0 +1,12 @@
+﻿function weatherForecastCtrl($scope) {
+
+    $scope.weather = {
+        location: "New York City",
+        status: "Cloudy",
+        temperature: "12 Celsius",
+        windSpeed: "25 km/h"
+    }
+
+}
+
+weatherForecastCtrl.$inject = ["$scope"];
