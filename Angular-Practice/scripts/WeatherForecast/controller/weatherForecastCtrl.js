5a351899a9018eca19effe6777360be2fc2b193b
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-21 13:26:52 +0000

    Added three more <app-info>, three more imagea and three more $scope, because I will include three more cities in the project

--- Angular-Practice/scripts/WeatherForecast/controller/weatherForecastCtrl.js
+++ Angular-Practice/scripts/WeatherForecast/controller/weatherForecastCtrl.js
@@ -2,7 +2,7 @@
 
     $scope.position = {
         "center-block"
-    }
+    };
 
     $scope.forecast = {
         "width": "300px",
@@ -10,12 +10,36 @@
         "font-size": "15px"
     };
 
-    $scope.weather = {
+    $scope.weatherNY = {
         image: "../../images/NYCweather.jpg",
         location: "New York City",
         status: "Cloudy",
         temperature: "12 Celsius",
         windSpeed: "25 km/h"
+    };
+
+    $scope.weatherLA = {
+        image: "../../images/LAweather.jpg",
+        location: "Los Angeles",
+        status: "Sunny",
+        temperature: "16 Celsius",
+        windSpeed: "15 km/h"
+    };
+
+    $scope.weatherSF = {
+        image: "../../images/SFCweather.jpg",
+        location: "San Francisco",
+        status: "Cloudy",
+        temperature: "18 Celsius",
+        windSpeed: "20 km/h"
+    };
+
+    $scope.weatherMIA = {
+        image: "../../images/MIAweather.jpg",
+        location: "Miami",
+        status: "Sunny",
+        temperature: "24 Celsius",
+        windSpeed: "7 km/h"
     };
 
 }
