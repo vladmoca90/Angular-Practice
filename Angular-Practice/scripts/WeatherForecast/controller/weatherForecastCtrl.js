c93f4e436212f68748944f8804a482ef40b158cd
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-22 00:28:37 +0000

    Added a list inline and going to create the code for the AngularJs

--- Angular-Practice/scripts/WeatherForecast/controller/weatherForecastCtrl.js
+++ Angular-Practice/scripts/WeatherForecast/controller/weatherForecastCtrl.js
@@ -1,36 +1,8 @@
 ﻿function weatherForecastCtrl($scope) {
 
-    $scope.weatherNY = {
-        image: "../../../images/NYCweather.jpg",
-        location: "New York City",
-        status: "Cloudy",
-        temperature: "12 Celsius",
-        windSpeed: "25 km/h"
-    };
+    $scope.cities = {
 
-    $scope.weatherLA = {
-        image: "../../../images/LAweather.jpg",
-        location: "Los Angeles",
-        status: "Sunny",
-        temperature: "16 Celsius",
-        windSpeed: "15 km/h"
-    };
-
-    $scope.weatherSF = {
-        image: "../../../images/SFCweather.jpg",
-        location: "San Francisco",
-        status: "Cloudy",
-        temperature: "18 Celsius",
-        windSpeed: "20 km/h"
-    };
-
-    $scope.weatherMIA = {
-        image: "../../../images/MIAweather.jpg",
-        location: "Miami",
-        status: "Sunny",
-        temperature: "24 Celsius",
-        windSpeed: "7 km/h"
-    };
+    }
 
 }
 
