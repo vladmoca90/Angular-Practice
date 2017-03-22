13fd13dc7c4ec6da96299d3592dd237eec3a2146
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-22 00:39:18 +0000

    Created the $scope code

--- Angular-Practice/scripts/WeatherForecast/controller/weatherForecastCtrl.js
+++ Angular-Practice/scripts/WeatherForecast/controller/weatherForecastCtrl.js
@@ -1,8 +1,11 @@
 ﻿function weatherForecastCtrl($scope) {
 
-    $scope.cities = {
-
-    }
+    $scope.cities = [
+        { picture: "../../../images/weatherForecast/NYCweather.jpg", name: "New York City", degrees: "18 Celsius", wind: "15km/h" },
+        { picture: "../../../images/weatherForecast/LAweather.jpg", name: "Los Angeles", degrees: "23 Celsius", wind: "14km/h" },
+        { picture: "../../../images/weatherForecast/SFweather.jpg", name: "San Francisco", degrees: "15 Celsius", wind: "23km/h" },
+        { picture: "../../../images/weatherForecast/MIAweather.jpg", name: "Miami", degrees: "29 Celsius", wind: "7km/h" },
+    ];
 
 }
 
